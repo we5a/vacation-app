eabfe33f536b45980ee25a11db3cfd90317beb8b
Yaroslav Soromlya <yaroslav.soromlya@ralabs.org>
2023-08-15 18:32:08 +0300

    Add Material UI

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import styles from "./App.module.scss";
 
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+
 function App() {
-  return <div className={styles.app}>Vacation app</div>;
+  return (
+    <div className={styles.app}>
+      <h2>Vacation app</h2>
+
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DateCalendar />
+      </LocalizationProvider>
+    </div>
+  );
 }
 
 export default App;

cf10d2939b411c34023a39860fa5baa5daa46e97
Yaroslav Soromlya <yaroslav.soromlya@ralabs.org>
2023-08-17 17:10:56 +0300

    Add logout menu layout

--- front-end/src/components/Header/Header.tsx
+++ front-end/src/components/Header/Header.tsx
@@ -1,17 +1,48 @@
-import type { FC } from "react";
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { FC, useState, MouseEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 import styles from "./Header.module.scss";
 
 const Header: FC = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const navigate = useNavigate();
+
+  const handleClick = (e: MouseEvent<HTMLElement>) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    navigate("/login");
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <h2 className={styles.appLogo}>Vacations</h2>
-        <div className={styles.userBlock}>
+
+        <div className={styles.userBlock} onClick={handleClick}>
           <span className={styles.userBlock__email}>user@email.com</span>
           <AccountCircleIcon />
         </div>
+        <Menu
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+        >
+          <MenuItem onClick={handleLogout}>Logout</MenuItem>
+        </Menu>
       </div>
     </div>
   );

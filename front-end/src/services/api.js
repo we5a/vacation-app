ee25154bc85150319dde851381941a5075a62401
Grygorii <nakan88@gmail.com>
2023-08-24 14:21:47 +0300

    set-up proxy

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,10 +1,24 @@
-const BASE_API_URL =
+export const BASE_API_URL2 =
   "http://ec2-34-242-160-104.eu-west-1.compute.amazonaws.com/vacationManagement";
+// for local needs
+export const BASE_API_URL = "http://localhost:8010/proxy/vacationManagement";
 
 export const getUsers = async () => {
   const response = await fetch(`${BASE_API_URL}/users`);
   const result = await response.json();
   return result._embedded.users;
+};
+
+export const createUser = async (userData) => {
+  const response = await fetch(`${BASE_API_URL}/users`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  });
+  const result = await response.json();
+  return result;
 };
 
 export const getUserById = async (userId) => {

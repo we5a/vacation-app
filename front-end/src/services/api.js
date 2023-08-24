5e9951fde338cf76bf897dc9a5c970540ddad055
Grygorii <nakan88@gmail.com>
2023-08-24 10:39:17 +0300

    remove proxy dependency

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,7 +1,20 @@
-const BASE_API_URL = "http://localhost:8010/proxy/vacationManagement";
+const BASE_API_URL =
+  "http://ec2-34-242-160-104.eu-west-1.compute.amazonaws.com/vacationManagement";
 
 export const getUsers = async () => {
   const response = await fetch(`${BASE_API_URL}/users`);
   const result = await response.json();
   return result._embedded.users;
+};
+
+export const getUserById = async (userId) => {
+  const response = await fetch(`${BASE_API_URL}/users/${userId}`);
+  const result = await response.json();
+  return result;
+};
+
+export const getVacationRequests = async () => {
+  const response = await fetch(`${BASE_API_URL}/vacationRequests`);
+  const result = await response.json();
+  return result._embedded.vacationRequests;
 };
